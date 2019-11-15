03ae1388c0e34ffcee61dcffa9838574870e6193
ddabberu <ddabberu@gmail.com>
2019-11-14 21:29:32 -0700

    Update test_countingvalleys.py

--- test_countingvalleys.py
+++ test_countingvalleys.py
@@ -2,5 +2,5 @@ from counting_valleys import countingValleys
 
 def test_count_valleys():
   assert countingValleys(8,"UDDDUDUU") == 1
-  assert countingValleys(12,DDUUDDUDUUUD) ==2
+  assert countingValleys(12,"DDUUDDUDUUUD") ==2
 
