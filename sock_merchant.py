305794188d361eeb20467615436a26fc7b83a6d8
ddabberu <ddabberu@gmail.com>
2019-11-14 20:13:38 -0700

    Update sock_merchant.py
    testing the workflow

--- sock_merchant.py
+++ sock_merchant.py
@@ -22,13 +22,8 @@ def sockMerchant(n, ar):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = sockMerchant(n, ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
